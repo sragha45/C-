5e6a04c3cbe5c0e145e25cf68ddefb1afde01ddb
Raghav <sragha45@gmail.com>
2018-11-01 14:07:01 +0530

    UPD: 1-11-18

---
+++ Contests/Codeforces/Random Practice/676C.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int n, int, string s) {
+}
+
+int main() {
+    int n, k;
+    string s;
+    cin >> n >> k >> s;
+    int start = 0, end = 0;
+    solve(n, k, s);
+}
